dc1b1692c46ed859b1b9dcc3c86020084d711e74
Prajwal <prajwalcr006@gmail.com>
2024-02-20 12:57:39 +0530

    2nd commit

--- myownapp/ui/theme/screen.kt
+++ myownapp/ui/theme/screen.kt
@@ -3,6 +3,7 @@ package com.example.myownapp.ui.theme
 import androidx.compose.foundation.horizontalScroll
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
@@ -13,9 +14,8 @@ class screen {
     fun title(modifier:Modifier = Modifier) {
         Column(
             modifier = modifier.size(2.dp)
-
         ) {
-
+            Text(text = "hello")
         }
     }
 }
